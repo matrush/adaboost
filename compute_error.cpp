669c78a2f4d372769603afd171b6de18ca8820c7
MatRush <lvchaoshuai@qq.com>
2013-11-17 18:59:33 -0800

    add computation of error

---
+++ compute_error.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstdio>
+#include <cmath>
+#include <cassert>
+#include <cstdlib>
+#include "adaboost.hpp"
+
+using namespace std;
+
+double compute_error(weak_classifier &classifier,
+                     vector<int> &feature_values,
+                     vector<double> &weights,
+                     int num_positive) {
+  double error = 0;
+  for (int i = 0; i < feature_values.size(); i++) {
+    // actual
+    int y = (i < num_positive) ? 1 : -1;
+    // classified
+    int h = classifier.h(feature_values[i]);
+    if (h != y) {
+      error += weights[i];
+    }
+  }
+  return error;
+  
+  // T+: total sum of positive sample weights
+  // T-: total sum of negative sample weights
+  double tplus = 0;
+  double tminus = 0;
+  for (int i = 0; i < num_positive; i++) {
+    tplus += weights[i];
+  }
+  for (int i = num_positive; i < weights.size(); i++) {
+    tminus += weights[i];
+  }
+  assert(fabs(tplus + tminus - 1) < 1e-8);
+  vector<int> indexes(feature_values.size());
+  for (int i = 0; i < indexes.size(); i++) {
+    indexes[i] = i;
+  }
+   // descending order
+  // get sorted index of feature_values
+  sort(indexes.begin(), indexes.end(), sort_proxy<int>(feature_values));
+
+  // S+: sum of positive sample weights below the threshold
+  // S-: sum of negative sample weights below the threshold
+  // e(x) = min((S+) + (T-) - (S-), (S-) + (T+) - (S+))
+  double splus = 0;
+  double sminus = 0;
+  double accumulate_sum_plus = 0;
+  double accumulate_sum_minus = 0;
+  int last_feature_value = -0x7FFFFFFF;
+  for (int i = indexes.size() - 1; i >= 0; i--) {
+    // update S+ and S-
+    double current_weight = weights[indexes[i]];
+    if (feature_values[indexes[i]] == last_feature_value) {
+      if (indexes[i] < num_positive) {
+        accumulate_sum_plus += current_weight;
+      } else {
+        accumulate_sum_minus += current_weight;
+      }
+    } else {
+      last_feature_value = feature_values[indexes[i]];
+      splus += accumulate_sum_plus;
+      sminus += accumulate_sum_minus;
+      accumulate_sum_plus = accumulate_sum_minus = 0;
+      if (indexes[i] < num_positive) {
+        accumulate_sum_plus += current_weight;
+      } else {
+        accumulate_sum_minus += current_weight;
+      }
+    }
+    double e1 = splus + tminus - sminus;
+    double e2 = sminus + tplus - splus;
+    if (feature_values[indexes[i]] == classifier.threshold) {
+      return min(e1, e2);
+    }
+  }
+  return 0.0;
+}
+
+double compute_error_real(weak_classifier &classifier,
+                     vector<int> &feature_values,
+                     vector<double> &weights,
+                     int num_positive) {
+  double pt = 0, qt = 0;
+  for (int i = 0; i < feature_values.size(); i++) {
+    // actual
+    int y = (i < num_positive) ? 1 : -1;
+    if (y == 1) {
+      pt += weights[i];
+    } else {
+      qt += weights[i];
+    }
+  }
+  classifier.weight = 0.5 * log(pt / qt);
+  double error = 2 * sqrt(pt * qt);
+  return error;
+}
